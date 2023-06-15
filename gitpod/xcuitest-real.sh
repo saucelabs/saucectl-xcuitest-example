580686f051d00ad8f1d24d3796582e31d9976c3e
Daniel Graham <daniel.graham@saucelabs.com>
2023-06-15 14:49:29 -0700

    make build and region configurable

--- gitpod/xcuitest-real.sh
+++ gitpod/xcuitest-real.sh
@@ -9,15 +9,23 @@ latest_tag=$(curl -s "https://api.github.com/repos/$app_repo/releases/latest" | 
 # APP AND TEST_APP. TO USE YOUR OWN, REPLACE THESE WITH A URL OR PATH TO YOUR OWN IPA AND TEST IPA
 export APP=https://github.com/$app_repo/releases/download/$latest_tag/SauceLabs-Demo-App.ipa
 export TEST_APP=https://github.com/$app_repo/releases/download/$latest_tag/SauceLabs-Demo-App-Runner.XCUITest.ipa
-export DATE_TIME=$(date +"%Y-%m-%d %H:%M:%S")
+
+# SET BUILD NAME
+DATE_TIME=$(date +"%Y-%m-%d %H:%M:%S")
+export DEFAULT_BUILD_NAME="Gitpod Build $DATE_TIME"
+: "${BUILD:=$DEFAULT_BUILD_NAME}"
+
+# SET REGION
+DEFAULT_REGION="us-west-1"
+export REGION=${REGION:=$DEFAULT_REGION}
 
 echo ""
 echo "======================================================"
 echo "Running Apple XCUITest tests on APP=$APP and TEST_APP=$TEST_APP in Sauce Labs cloud!"
 echo ""
-echo "To vew your test results in real time visit https://app.saucelabs.com/dashboard/builds/rdc and open the build with name 'Gitpod Build $DATE_TIME'"
+echo "To vew your test results in real time visit https://app.$REGION.saucelabs.com/dashboard/builds/rdc and open the build with name 'Gitpod Build $DATE_TIME'"
 echo "======================================================"
 echo ""
 sleep 8
 
-./bin/saucectl run
+./bin/saucectl run --region $REGION --build "$BUILD"
